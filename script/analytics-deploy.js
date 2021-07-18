14a2914cdd32433dadbe4082cce8b0dd8133fcb3
Anurag <anuraglama5@gmail.com>
2021-07-18 21:37:35 +0530

    Delete analytics-deploy.js

--- script/analytics-deploy.js
+++
@@ -1,6 +0,0 @@
-self.importScripts('/script/analytics-interface.js');
-self.addEventListener('notificationclose', function(event) {
-  event.waitUntil(
-    sendAnalyticsEvent('close', 'notification')
-  );
-});
