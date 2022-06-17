a2d862e0eb76034c83e1e1d945cfda83bac2fff1
Anurag <anuraglama5@gmail.com>
2022-06-17 21:47:49 +0530

    Add files via upload

--- anxsion-serviceworker.js
+++ anxsion-serviceworker.js
@@ -1,30 +1,78 @@
-const CACHE = "ANXSION-dataCache";
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
-);
+const HTML_CACHE = "html";
+const JS_CACHE = "javascript";
+const STYLE_CACHE = "stylesheets";
+const IMAGE_CACHE = "images";
+const FONT_CACHE = "fonts";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
-  event.waitUntil(
-    caches
-      .keys()
-      .then(keys => keys.filter(key => key !== CACHE))
-      .then(keys =>
-        Promise.all(
-          keys.map(key => {
-            return caches.delete(key);
-          })
-        )
-      )
-  );
 });
 
 workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.CacheFirst({
-    cacheName: CACHE
+  ({event}) => event.request.destination === 'document',
+  new workbox.strategies.NetworkFirst({
+    cacheName: HTML_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 10,
+        maxSecondsmaxAgeSeconds: 60,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({event}) => event.request.destination === 'script',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: JS_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+        maxSecondsmaxAgeSeconds: 60,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({event}) => event.request.destination === 'style',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: STYLE_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+        maxSecondsmaxAgeSeconds: 60,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({event}) => event.request.destination === 'image',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: IMAGE_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+        maxSecondsmaxAgeSeconds: 60,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({event}) => event.request.destination === 'font',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: FONT_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+        maxSecondsmaxAgeSeconds: 60,
+      }),
+    ],
   })
 );
