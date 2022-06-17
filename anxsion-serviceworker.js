3b37caa2338e1bd7c9be5cb34914a4a4d7fa05f6
Anurag <anuraglama5@gmail.com>
2022-06-17 21:30:21 +0530

    Add files via upload

--- anxsion-serviceworker.js
+++ anxsion-serviceworker.js
@@ -3,16 +3,11 @@ const CACHE = "ANXSION-dataCache";
 importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
-import {BackgroundSyncPlugin} from 'workbox-background-sync';
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
-});
-
-const bgSyncPlugin = new BackgroundSyncPlugin('ANXSION-bgSync', {
-  maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
 });
 
 workbox.routing.registerRoute(
