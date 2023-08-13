a8229dad07a8cd1e9b2341684825ec445aa8e72a
AnxSion <anuraglama5@gmail.com>
2023-08-14 00:07:19 +0530

    Update

--- script/chrysalis.js
+++ script/chrysalis.js
@@ -1,12 +1,13 @@
 // Chrysalis Engine 1.0
 
 let qnaModelPromise;
+let verb;
 
 let passages = {
-    "identity": "I am Chrysalis! A conversational neural model and an assistant AI.",
-    "greeting": "Hello! I'm here to answer your questions.",
+    "identity": "Chrysalis is a conversational neural model and an assistant AI. Chrysalis is designed by Anurag Lama (ANXSION). Chrysalis was made to become a base template for the future pre-trained models.",
+    "greeting": "Hello! How can I help you?",
     "apology": "I apologize for not being able to provide an answer at the moment.",
-    "document": "Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple, and Facebook. Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002. An initial public offering (IPO) took place on August 19, 2004, and Google moved to its headquarters in Mountain View, California, nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various interests as a conglomerate called Alphabet Inc. Google is Alphabet's leading subsidiary and will continue to be the umbrella company for Alphabet's Internet interests. Sundar Pichai was appointed CEO of Google, replacing Larry Page who became the CEO of Alphabet."
+    "document": "ANXSION is an organisation dedicated to the creation of innovative solutions that resonate with the betterment of society. It was founded in 2018. Anurag Lama is the founder of ANXSION. ANXSION has 2 distinctive organisations: Guardian Angel, founded in 2022, a full service law firm that stands as a beacon of legal support, and Dendritic Dynamics, founded in 2023, a pioneering AI Research and Development initiative that fervently drives scientific progress and prototyping. 'Chrysalis' is the the base template of neural model created by Dendritic Dynamics. Chrysalis was made August 13, 2023."
   }
 
   async function getQnAModel() {
@@ -24,32 +25,50 @@ let passages = {
   async function getAnswer() {
     const questionInput = document.getElementById('questionInput');
     const answerOutput = document.getElementById('answerOutput');
-    answerOutput.innerHTML = 'Thinking...';
     
     const question = questionInput.value;
+    document.getElementById('questionInput').innerText="";
     const passage = passages.document;
+    const selfid = passages.identity;
   
     try {
-      const qnaModel = await getQnAModel();      
-      const answers = await qnaModel.findAnswers(question, passage.document);
+        answerOutput.innerHTML = 'Thinking...';
+        const qnaModel = await getQnAModel(); 
+        const answers = await qnaModel.findAnswers(question, passage);
+        const identity = await qnaModel.findAnswers(question, selfid);
 
-      if (question.toLowerCase().includes("who are you") || question.toLowerCase().includes("what are you")) {
-        displayAnswer(passages.identity);
-      }
-      else if (answers.length > 0) {
-        displayAnswer(answers[0].text);
-      } 
-      else {
-        if (passages.apology) {
-          displayAnswer(passages.apology);
-        } else {
-          displayAnswer('I\'m sorry, I don\'t have an answer for that.');
+
+        if (question.toLowerCase().includes('designed you') || question.toLowerCase().includes('made you') || question.toLowerCase().includes('created you')){
+            displayAnswer("I was designed by "+identity[0].text+".");
         }
-      }
-    } catch (error) {
-      console.error('Error loading or using the Q&A model:', error);
+        else if (question.toLowerCase().includes('when were you')){
+            displayAnswer("I was designed on "+answers[0].text+".");
+        }
+        else if (question.toLowerCase().includes('what are you') || question.toLowerCase().includes('who are you')){
+            displayAnswer("I am conversational neural model and an assistant AI.");
+        }
+        else if (question.toLowerCase().includes('why were you made')){
+            displayAnswer("I was developed " + identity[0].text+".");
+        }
+        else if (question.toLowerCase().includes('why are you')){
+            displayAnswer("Why?");
+        }
+        else if (answers.length > 0) {
+            displayAnswer(answers[0].text);
+        } 
+        else {
+            if (passages.apology) {
+            displayAnswer(passages.apology);
+            } else {
+            displayAnswer('I\'m sorry, I don\'t have an answer for that.');
+            }
+        }
+        } 
+        catch (error) {
+            displayAnswer('An unknow error occured. Can you try something else?');
+            console.error('Error loading or using the Q&A model:', error);
+        }
     }
-  }
   
   function displayAnswer(text) {
     const answerOutput = document.getElementById('answerOutput');
