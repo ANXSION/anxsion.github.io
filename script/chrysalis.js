836e5de3f69885ce0fcffa7471713f809d9d7ec6
AnxSion <anuraglama5@gmail.com>
2023-08-22 22:25:51 +0530

    Majora

--- script/chrysalis.js
+++ script/chrysalis.js
@@ -1,9 +1,19 @@
 async function getAnswer(){
-  const MODEL_URL = './script/neural/chrysalis/model.json';
 
+  // Load the TensorFlow model.
+  const model = await tf.loadLayersModel('./script/neural/chrysalis/model.json');
 
+  // Get the input text from the user.
+  const questionInput = document.getElementById('questionInput');
+  const question = questionInput.value;
 
-  const model = await tf.loadLayersModel(MODEL_URL);
-  const cat = document.getElementById('questionInput').value;
-  model.predict(tf.tensor(cat));
+  // Tokenize the input text.
+  const tokens = universalSentenceEncoder.encode(question);
+
+  // Get the answer from the model.
+  const answer = model.predict(tokens);
+
+  // Set the output text.
+  const answerOutput = document.getElementById('answeroutput');
+  answerOutput.innerHTML = answer;
 }
