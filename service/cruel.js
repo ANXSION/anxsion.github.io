a999998aa259e3c3d6ebdeefb04b38dfbf323c06
AnxSion <anuraglama5@gmail.com>
2024-04-09 09:28:10 +0530

    Major Update

--- service/cruel.js
+++ service/cruel.js
@@ -1,9 +1,9 @@
 let propertyData = [
-    ["01/2023",'Hatigisha #01', 'Plot with structure', '1_2023_cover.jpg','No data', ['7 Katha','Bastu','General', 'Freehold','12 ft','Residential','3BHK','G+1', '-', 'Available', 'Under progress', 'none'], ['0m','0m','0m','0m','0m','0m','0m'],[4500000,'Mortgaged']],
-    ["02/2023",'Pradhan Nagar #01', 'Apartment', '2_2023_cover.jpg','No data', ['Residential','2BHK','1st', '1200 Sq. ft.','12 ft','Available','2012','None'], ['0m','0m','0m','0m','0m','0m','0m'],[4500000,'Clear']],
-    ["03/2023",'Pradhan Nagar #02', 'Apartment', '3_2023_cover.jpg','No data', ['Residential','3BHK','3rd', '1200 Sq. ft.','12 ft','Available','2013','None'], ['0m','0m','0m','0m','0m','0m','0m'],[3600000,'Clear']],
-    ["01/2024",'Gossaipur #01', 'Plot', '1_2024_cover.jpg','No data', ['2.5 Bigha','Rupni','General', 'Freehold','14 ft','Gram Panchayat'], ['0m','0m','0m','0m','0m','0m','0m'],[17500000,'Clear']],
-    ["02/2024",'Pradhan Nagar #03', 'Apartment', '2_2024_cover.jpg',['2_2024_1.jpg', '2_2024_2.jpg', '2_2024_3.jpg','2_2024_4.jpg'], ['Residential','3BHK','2nd', '-','12 ft','unavailable','2016','None'], ['0m','0m','0m','0m','0m','0m','0m'],[3800000,'Clear']]
+    ['01/2023','Hatigisha #01', 'Plot with structure', '1_2023_cover.jpg', 'No data', [5040, 'Bastu', 'General', 'Freehold','12 ft','Residential','3BHK','G+1', '-', 'Available', 'Under construction', 'none'], ['0m','0m','0m','0m','0m','0m','0m'],[4500000,'Financed']],
+    ['02/2023','Pradhan Nagar #01', 'Apartment', '2_2023_cover.jpg','No data', [1200, 'Residential', '2BHK', '1st', '12 ft', 'Available', 2012, 'None'], ['0m','0m','0m','0m','0m','0m','0m'],[4500000,'None']],
+    ['03/2023','Pradhan Nagar #02', 'Apartment', '3_2023_cover.jpg','No data', [1200, 'Residential', '3BHK', '3rd', '12 ft', 'Available', 2013, 'None'], ['0m','0m','0m','0m','0m','0m','0m'],[3600000,'None']],
+    ['01/2024','Gossaipur #01', 'Plot', '1_2024_cover.jpg','No data', [36000,'Rupni','General', 'Freehold','14 ft','Gram Panchayat'], ['0m','0m','0m','0m','0m','0m','0m'],[17500000,'None']],
+    ['02/2024','Pradhan Nagar #03', 'Apartment', '2_2024_cover.jpg',['2_2024_1.jpg', '2_2024_2.jpg', '2_2024_3.jpg','2_2024_4.jpg'], [1200, 'Residential','3BHK','2nd','12 ft','unavailable',2016,'None'], ['0m','0m','0m','0m','0m','0m','0m'],[3800000,'Financed']]
 ];
 
 const dateEvents = new Date();
@@ -15,21 +15,23 @@ const options = {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
-  };
+};
 
 let brandName = "Tyrant's Hold";
 let copyYear = dateEvents.getFullYear();
 
-let injectedData, injectImage, injectUpperdata, injectLowerdata, injectSpecifications, injectContracts, contractData;
+let injectedData, injectImage, injectUpperdata, injectLowerdata, injectSpecifications, injectContracts, ageData, contractData;
+let sizeFactor = 720;
 
 let nearby_Parameters = ['School', 'Hospital', 'Commerce', 'Highway', 'Railway', 'Airport', 'Seaport'];
-let financial_Parameters = ['Price', 'Encumbrance'];
-let contract_Parameters = ['Purchase Date', 'Prepayment', 'Sale Price', 'Ask Price'];
+let financial_Parameters = ['Base Price', 'Per unit', 'Encumbrance'];
+let contract_Parameters = ['Purchase Date', 'Booking Amount', 'Sale Price', 'Ask Price'];
 
-let plotwithstructure_Parameters = ['Size','Classification','Owner', 'Ownership','Road', 'Use', 'Layout', 'Floors', 'Area', 'Parking', 'Built', 'Revovation'];
-let plot_Parameters = ['Size','Classification','Owner', 'Ownership','Road','Authority'];
-let apartment_Parameters = ['Use', 'Layout', 'Floor', 'Area', 'Road', 'Parking', 'Built', 'Revovation'];
+let sizeMetric = ['Sq. ft.', 'Katha', 'Decimal', 'Biga', 'Acre'];
 
+let plotwithstructure_Parameters = ['Size', 'Classification', 'Owner', 'Ownership', 'Road', 'Use', 'Layout', 'Floors', 'Area', 'Parking', 'Built', 'Revovation'];
+let plot_Parameters = ['Size', 'Classification', 'Owner', 'Ownership', 'Road', 'Authority'];
+let apartment_Parameters = ['Size', 'Use', 'Layout', 'Floor', 'Road', 'Parking', 'Built', 'Revovation'];
 
 contractDateShort.setDate(contractDateShort.getDate()+ 7);
 contractDateMid.setDate(contractDateMid.getDate()+ 15);
@@ -61,7 +63,6 @@ for (let cycle = 0; cycle < propertyData.length; cycle++){
             Math.round(propertyData[cycle][7][0] - (0.01 * propertyData[cycle][7][0])),
             propertyData[cycle][7][0]
         ],
-
         propertyData[cycle][7][0]
     ];
 
@@ -124,7 +125,8 @@ for (let cycle = 0; cycle < propertyData.length; cycle++){
     <h1>Financials</h1>
     <div class="dataGrid">
         <p>`+financial_Parameters[0]+`: `+propertyData[cycle][7][0]+`</p>
-        <p>`+financial_Parameters[1]+`: `+propertyData[cycle][7][1]+`</p>
+        <p>`+financial_Parameters[1]+`: `+ Math.round(propertyData[cycle][7][0] / propertyData[cycle][5][0])+` / `+ sizeMetric[0] +`</p>
+        <p>`+financial_Parameters[2]+`: `+propertyData[cycle][7][1]+`</p>
     </div>
     `;
 
@@ -137,6 +139,7 @@ for (let cycle = 0; cycle < propertyData.length; cycle++){
         </div>
         `;
     }
+
     else{
         injectImage=`
         <h1>Visuals</h1>
@@ -150,11 +153,18 @@ for (let cycle = 0; cycle < propertyData.length; cycle++){
     }
 
     if(propertyData[cycle][2] == 'Plot with structure'){
-        
-        injectedData = `
+
+        if(propertyData[cycle][5][10]=='Under construction'){
+            ageData = propertyData[cycle][5][10];
+        }
+        else{
+            ageData = dateEvents.getFullYear() - propertyData[cycle][5][10];
+        }
+
+        injectSpecifications = `
         <h1>Land Specifications</h1>
         <div class="dataGrid">
-            <p>`+plotwithstructure_Parameters[0]+`: `+propertyData[cycle][5][0]+`</p>
+            <p>`+plotwithstructure_Parameters[0]+`: `+ Math.round(propertyData[cycle][5][0])+ ` Sq. ft. (` +(propertyData[cycle][5][0] / sizeFactor)+` `+ sizeMetric[1] +`)`+`</p>
             <p>`+plotwithstructure_Parameters[1]+`: `+propertyData[cycle][5][1]+`</p>
             <p>`+plotwithstructure_Parameters[2]+`: `+propertyData[cycle][5][2]+`</p>
             <p>`+plotwithstructure_Parameters[3]+`: `+propertyData[cycle][5][3]+`</p>
@@ -169,18 +179,19 @@ for (let cycle = 0; cycle < propertyData.length; cycle++){
             <p>`+plotwithstructure_Parameters[7]+`: `+propertyData[cycle][5][7]+`</p>
             <p>`+plotwithstructure_Parameters[8]+`: `+propertyData[cycle][5][8]+`</p>
             <p>`+plotwithstructure_Parameters[9]+`: `+propertyData[cycle][5][9]+`</p>
-            <p>`+plotwithstructure_Parameters[10]+`: `+propertyData[cycle][5][10]+`</p>
+            <p>`+plotwithstructure_Parameters[10]+`: `+ageData+`</p>
             <p>`+plotwithstructure_Parameters[11]+`: `+propertyData[cycle][5][11]+`</p>
         </div>
         `;   
     }
+    
 
     else if(propertyData[cycle][2] == 'Plot'){
 
-        injectedData = `
+        injectSpecifications = `
         <h1>Land Specifications</h1>
         <div class="dataGrid">
-            <p>`+plot_Parameters[0]+`: `+propertyData[cycle][5][0]+`</p>
+            <p>`+plot_Parameters[0]+`: `+ Math.round(propertyData[cycle][5][0])+ ` Sq. ft. (` +(propertyData[cycle][5][0] / sizeFactor)+` `+ sizeMetric[1] +`)`+`</p>
             <p>`+plot_Parameters[1]+`: `+propertyData[cycle][5][1]+`</p>
             <p>`+plot_Parameters[2]+`: `+propertyData[cycle][5][2]+`</p>
             <p>`+plot_Parameters[3]+`: `+propertyData[cycle][5][3]+`</p>
@@ -192,6 +203,13 @@ for (let cycle = 0; cycle < propertyData.length; cycle++){
 
     else if(propertyData[cycle][2] == 'Apartment'){
 
+        if(propertyData[cycle][5][10]=='Under construction'){
+            ageData = propertyData[cycle][5][10];
+        }
+        else{
+            ageData = dateEvents.getFullYear() - propertyData[cycle][5][6];
+        }
+
         injectSpecifications = `
         <h1>Unit Specifications</h1>
         <div class="dataGrid">
@@ -201,7 +219,8 @@ for (let cycle = 0; cycle < propertyData.length; cycle++){
             <p>`+apartment_Parameters[3]+`: `+propertyData[cycle][5][3]+`</p>
             <p>`+apartment_Parameters[4]+`: `+propertyData[cycle][5][4]+`</p>
             <p>`+apartment_Parameters[5]+`: `+propertyData[cycle][5][5]+`</p>
-            <p>`+apartment_Parameters[6]+`: `+propertyData[cycle][5][6]+`</p>
+            <p>`+apartment_Parameters[6]+`: `+ageData+` Years</p>
+            <p>`+apartment_Parameters[7]+`: `+propertyData[cycle][5][7]+`</p>
         </div>
         `;   
     }
@@ -219,7 +238,7 @@ for (let cycle = 0; cycle < propertyData.length; cycle++){
             `+injectContracts+`
             <br><br>
         </details>
-        `;   
-    
+        `;
+
     document.getElementById('mainListing').insertAdjacentHTML('beforeend', injectedData);
 }
