411605947bb35c1a459e40780fd0935aae1788fc
AnxSion <anuraglama5@gmail.com>
2024-04-14 22:59:52 +0530

    Update

--- project/archlex/archlex.js
+++ project/archlex/archlex.js
@@ -1,3 +1,3 @@
 document.getElementById('logoInject').insertAdjacentHTML("beforeend",`
-    <p style="font-size: 1rem; letter-spacing: 9px; margin: 0; color: white; text-align: center"><span style="border-bottom: 2px white solid; padding-bottom: 6px;">Arch</span><span style="border-top: 2px white solid; border-right: 2px white solid; border-bottom: 2px white solid; padding: 6px;">LEX<span></p>
+    <p style="font-size: 1.5rem; letter-spacing: 9px; margin: 0; color: white; text-align: center"><span style="border-bottom: 2px white solid; padding-bottom: 6px;">Arch</span><span style="border-top: 2px white solid; border-right: 2px white solid; border-bottom: 2px white solid; padding: 6px;">LEX<span></p>
 `);
