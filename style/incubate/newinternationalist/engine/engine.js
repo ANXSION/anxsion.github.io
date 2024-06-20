af88856eb796db02437c5bc2b675b59fa2bda5e1
AnxSion <anuraglama5@gmail.com>
2024-06-21 01:23:47 +0530

    update Nav

--- style/incubate/newinternationalist/engine/engine.js
+++ style/incubate/newinternationalist/engine/engine.js
@@ -1,24 +1,27 @@
-document.getElementById('navMobileDisplay').style.top = '-100%';
+let menuStatus = 0;
 function menuOpen(){
     document.getElementById('menuOpen').style.display = 'none';
     document.getElementById('menuClose').style.display = 'flex';
     document.getElementById('navMobileDisplay').style.top = '80px';
+    menuStatus = 1;
 }
 
 function menuClose(){
     document.getElementById('menuOpen').style.display = 'flex';
     document.getElementById('menuClose').style.display = 'none';
     document.getElementById('navMobileDisplay').style.top = '-100%';
+    menuStatus = 0;
 }
-
 
 var prevScrollpos = window.scrollY ;
 window.onscroll = function() {
   var currentScrollPos = window.scrollY ;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("nav").style.top = "0";
-  } else {
-    document.getElementById("nav").style.top = "-100%";
+  if (menuStatus != 1){
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("nav").style.top = "0";
+      } else {
+        document.getElementById("nav").style.top = "-100%";
+      }
   }
   prevScrollpos = currentScrollPos;
 }
