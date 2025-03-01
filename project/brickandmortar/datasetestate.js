af58572c4250f8dea0aa8fd714fb4def52184a5a
AnxSion <anuraglama5@gmail.com>
2025-03-01 23:46:33 +0530

    Update

---
+++ project/brickandmortar/datasetestate.js
@@ -0,0 +1,102 @@
+const plotdata=[
+    {
+        "assetType": "Vacant Plot",
+        "transactionType": "For Sale",
+        "assetID": "PLOT 001/24",
+        "salePrice": 4750000,
+        "dataCluster":{
+            "cluster1": "Hatigisha",
+            "cluster2": "Location",
+            "cluster3": "2.5",
+            "cluster4": "Bigha",
+            "cluster5": "14 ft.",
+            "cluster6": "Kutcha road.",
+            "cluster7": "Schedule Tribe",
+            "cluster8": "2005 (Owned)"
+        },
+        "neighbourhoodDetails":{
+            "distanceFromCommercial": 1.5,
+            "distanceFromPark": 0,
+            "distanceFromSchool": 2.7,
+            "distanceFromHospital": 5,
+            "distanceFromHighway": 0.7,
+            "distanceFromStation": 8,
+            "distanceFromAirport": 8
+        }
+    },
+    {
+        "assetType": "Apartment",
+        "transactionType": "For Sale",
+        "assetID": "APARTMENT 001/23",
+        "salePrice": 2300000,
+        "dataCluster":{
+            "cluster1": "Shivmandir",
+            "cluster2": "Location",
+            "cluster3": "1150",
+            "cluster4": "sq. ft (No parking)",
+            "cluster5": "3BHK",
+            "cluster6": "1st Floor",
+            "cluster7": "General",
+            "cluster8": "2018 (Built)"
+        },
+        "neighbourhoodDetails":{
+            "distanceFromCommercial": 0.1,
+            "distanceFromPark": 0.1,
+            "distanceFromSchool": 0.2,
+            "distanceFromHospital": 0.5,
+            "distanceFromHighway": 0.1,
+            "distanceFromStation": 3,
+            "distanceFromAirport": 5
+        }
+    },
+    {
+        "assetType": "Apartment",
+        "transactionType": "For Sale",
+        "assetID": "APARTMENT 001/25",
+        "salePrice": 4800000,
+        "dataCluster":{
+            "cluster1": "Siliguri",
+            "cluster2": "Location",
+            "cluster3": "1000",
+            "cluster4": "sq. ft (350 sq. ft. Parking)",
+            "cluster5": "2BHK",
+            "cluster6": "2nd Floor",
+            "cluster7": "General",
+            "cluster8": "2012 (Built)"
+        },
+        "neighbourhoodDetails":{
+            "distanceFromCommercial": 0.1,
+            "distanceFromPark": 0.2,
+            "distanceFromSchool": 0.5,
+            "distanceFromHospital": 0.5,
+            "distanceFromHighway": 0.1,
+            "distanceFromStation": 5,
+            "distanceFromAirport": 12
+        }
+    },
+    {
+        "assetType": "Building",
+        "transactionType": "For Sale",
+        "assetID": "BUILDING 01/22",
+        "salePrice": 2900000,
+        "dataCluster":{
+            "cluster1":"Hatigisha",
+            "cluster2": "Location",
+            "cluster3": "3",
+            "cluster4": "Kattha",
+            "cluster5": "G+1",
+            "cluster6": "Foundation",
+            "cluster7": "General",
+            "cluster8": "2020 (Built)"
+        },
+        "neighbourhoodDetails":{
+            "distanceFromCommercial": 0.5,
+            "distanceFromPark": 2,
+            "distanceFromSchool": 1,
+            "distanceFromHospital": 5,
+            "distanceFromHighway": 1,
+            "distanceFromStation": 6,
+            "distanceFromAirport": 6
+        }
+    }
+];
