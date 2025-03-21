7cf9cdfad0dae952aef9aef41feb0c87928da446
AnxSion <anuraglama5@gmail.com>
2025-03-21 10:43:03 +0530

    Update property

--- project/nestquest/datasetestate.js
+++ project/nestquest/datasetestate.js
@@ -1,110 +1,101 @@
 const plotdata=[
     {
-        "auction": "no",
         "assetType": "Vacant Plot",
         "transactionType": "For Sale",
         "assetID": "PLOT 001/24",
         "salePrice": 4750000,
         "dataCluster":{
-            "cluster1": "Hatigisha",
-            "cluster2": "Location",
-            "cluster3": "2.5",
-            "cluster4": "Bigha",
-            "cluster5": "14 ft.",
-            "cluster6": "Kutcha road.",
-            "cluster7": "Schedule Tribe",
-            "cluster8": "2005 (Owned)"
-        },
-        "neighbourhoodDetails":{
-            "distanceFromCommercial": 1.5,
-            "distanceFromPark": 0,
-            "distanceFromSchool": 2.7,
-            "distanceFromHospital": 5,
-            "distanceFromHighway": 0.7,
-            "distanceFromStation": 8,
-            "distanceFromAirport": 8
+            "cluster1": {
+                "clusterCategory":"Location",
+                "clusterValue":"Matigara",
+            },
+            "cluster2": {
+                "clusterCategory":"Size",
+                "clusterValue":"1.3 Biga",
+            },
+            "cluster3": {
+                "clusterCategory":"Ownership",
+                "clusterValue":"Scheduled Tribe",
+            },
+            "cluster4": {
+                "clusterCategory":"Classification",
+                "clusterValue":"Rupni",
+            },
+            "cluster5": {
+                "clusterCategory":"Road Access",
+                "clusterValue":"12ft (North)",
+            },
+            "cluster6": {
+                "clusterCategory":"-",
+                "clusterValue":"-",
+            }
         }
     },
     {
-        "auction": "no",
-        "assetType": "Apartment",
+        "assetType": "Flat",
         "transactionType": "For Sale",
-        "assetID": "APARTMENT 001/23",
-        "salePrice": 2300000,
+        "assetID": "FLAT 002/24",
+        "salePrice": 2200000,
         "dataCluster":{
-            "cluster1": "Shivmandir",
-            "cluster2": "Location",
-            "cluster3": "1150",
-            "cluster4": "sq. ft",
-            "cluster5": "3BHK + No parking",
-            "cluster6": "1st Floor",
-            "cluster7": "General",
-            "cluster8": "2018 (Built)"
-        },
-        "neighbourhoodDetails":{
-            "distanceFromCommercial": 0.1,
-            "distanceFromPark": 0.1,
-            "distanceFromSchool": 0.2,
-            "distanceFromHospital": 0.5,
-            "distanceFromHighway": 0.1,
-            "distanceFromStation": 3,
-            "distanceFromAirport": 5
+            "cluster1": {
+                "clusterCategory":"Location",
+                "clusterValue":"Shivmandir",
+            },
+            "cluster2": {
+                "clusterCategory":"Overall Size",
+                "clusterValue":"1175 sq. ft.",
+            },
+            "cluster3": {
+                "clusterCategory":"Ownership",
+                "clusterValue":"General",
+            },
+            "cluster4": {
+                "clusterCategory":"Built",
+                "clusterValue":"2018",
+            },
+            "cluster5": {
+                "clusterCategory":"Floor Location",
+                "clusterValue":"3rd Floor",
+            },
+            "cluster6": {
+                "clusterCategory":"Parking",
+                "clusterValue":"Available",
+            }
         }
     },
     {
-        "auction": "no",
-        "assetType": "Apartment",
-        "transactionType": "For Sale",
-        "assetID": "APARTMENT 001/25",
-        "salePrice": 4800000,
-        "dataCluster":{
-            "cluster1": "Siliguri",
-            "cluster2": "Location",
-            "cluster3": "1000",
-            "cluster4": "sq. ft",
-            "cluster5": "2BHK + Parking",
-            "cluster6": "2nd Floor",
-            "cluster7": "General",
-            "cluster8": "2012 (Built)"
-        },
-        "neighbourhoodDetails":{
-            "distanceFromCommercial": 0.1,
-            "distanceFromPark": 0.2,
-            "distanceFromSchool": 0.5,
-            "distanceFromHospital": 0.5,
-            "distanceFromHighway": 0.1,
-            "distanceFromStation": 5,
-            "distanceFromAirport": 12
-        }
-    },
-    {
-        "auction": "yes",
         "assetType": "Building",
         "transactionType": "For Sale",
-        "assetID": "BUILDING 01/22",
-        "salePrice": 2900000,
+        "assetID": "BULD 003/24",
+        "salePrice": 3800000,
         "dataCluster":{
-            "cluster1":"Hatigisha",
-            "cluster2": "Location",
-            "cluster3": "3",
-            "cluster4": "Kattha",
-            "cluster5": "G+1",
-            "cluster6": "Foundation",
-            "cluster7": "General",
-            "cluster8": "2020 (Built)"
-        },
-        "neighbourhoodDetails":{
-            "distanceFromCommercial": 0.5,
-            "distanceFromPark": 2,
-            "distanceFromSchool": 1,
-            "distanceFromHospital": 5,
-            "distanceFromHighway": 1,
-            "distanceFromStation": 6,
-            "distanceFromAirport": 6
+            "cluster1": {
+                "clusterCategory":"Location",
+                "clusterValue":"Hatigisha",
+            },
+            "cluster2": {
+                "clusterCategory":"Overall Size",
+                "clusterValue":"7 Kattha",
+            },
+            "cluster3": {
+                "clusterCategory":"Ownership",
+                "clusterValue":"General",
+            },
+            "cluster4": {
+                "clusterCategory":"Built",
+                "clusterValue":"2018",
+            },
+            "cluster5": {
+                "clusterCategory":"Road Access",
+                "clusterValue":"12ft. (North)",
+            },
+            "cluster6": {
+                "clusterCategory":"Foundation",
+                "clusterValue":"G+1",
+            }
         }
     }
-];
-
+]
 
 let demanddata= [
     {
@@ -114,14 +105,38 @@ let demanddata= [
         "assetID": "APARTMENT 001/25",
         "maxPrice": 12000,
         "dataCluster":{
-            "cluster1": "Gangtok",
-            "cluster2": "Location",
-            "cluster3": "Any",
-            "cluster4": "Size",
-            "cluster5": "Residential",
-            "cluster6": "Purpose",
-            "cluster7": "None",
-            "cluster8": "Other criteria"
+            "cluster1": {
+                "ClusterCategory":"Ownership",
+                "clusterValue":"Scheduled Tribe",
+            },
+            "cluster2": {
+                "ClusterCategory":"Ownership",
+                "clusterValue":"Scheduled Tribe",
+            },
+            "cluster3": {
+                "ClusterCategory":"Ownership",
+                "clusterValue":"Scheduled Tribe",
+            },
+            "cluster4": {
+                "ClusterCategory":"Ownership",
+                "clusterValue":"Scheduled Tribe",
+            },
+            "cluster5": {
+                "ClusterCategory":"Ownership",
+                "clusterValue":"Scheduled Tribe",
+            },
+            "cluster6": {
+                "ClusterCategory":"Ownership",
+                "clusterValue":"Scheduled Tribe",
+            },
+            "cluster7": {
+                "ClusterCategory":"Ownership",
+                "clusterValue":"Scheduled Tribe",
+            },
+            "cluster8": {
+                "ClusterCategory":"Ownership",
+                "clusterValue":"Scheduled Tribe",
+            },
         }
     }
 ]
